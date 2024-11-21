4f53db949830987de7b7b1b20dfc6721f42c03f5
coriger <461504978@qq.com>
2024-11-21 08:03:36 +0800

    init

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,107 +1,97 @@
 import { defineUserConfig } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
 import { viteBundler } from '@vuepress/bundler-vite'
-import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
-  title: "",
-  description: "",
-  head: [
-    ['link', { rel: 'icon', href: 'logo.png' }],
-  ],
-  bundler: viteBundler(),
-  theme: recoTheme({
-    style: "@vuepress-reco/style-default",
-    logo: "/logo.png",
-    author: "coriger",
-    authorAvatar: "/head.png",
-    docsRepo: "https://github.com/coriger/supr-blog",
-    docsBranch: "main",
-    docsDir: "/docs",
-    lastUpdatedText: "",
-    // 设置语言
-    lang: "zh-CN",
-    // 自动设置分类
-    autoSetSeries: true,
-    navbar: [
-      { text: "Home", link: "/" },
-      { text: "Categories", link: "/categories/Java/1.html" },
-      { text: "Tags", link: "/tags/jishu/1.html" },
-      {
-        text: "Docs",
-        children: [
-          { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
-          { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
-        ],
-      },
+    title: "supr-blog",
+    description: "",
+    head: [
+        ['link', { rel: 'icon', href: 'logo.png' }],
     ],
-    bulletin: {
-      body: [
-        {
-          type: "text",
-          content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "QQ 群",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li>QQ群1：1037296104</li>
-            <li>QQ群2：1061561395</li>
-            <li>QQ群3：962687802</li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "GitHub",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "buttongroup",
-          children: [
+    bundler: viteBundler(),
+    theme: recoTheme({
+        style: "@vuepress-reco/style-default",
+        logo: "/logo.png",
+        author: "coriger",
+        authorAvatar: "/head2.png",
+        docsBranch: "master",
+        docsDir: "./docs",
+        colorMode: 'dark', // dark, light, 默认 auto
+        tip: '提示',
+        info: '信息',
+        danger: '危险',
+        warning: '警告',
+        details: '详情',
+        tagsText: '标签',
+        backToHome: '返回首页',
+        categoriesText: '分类',
+        catalogTitle: '页面导航',
+        editLinkText: '编辑当前页面',
+        lastUpdatedText: '最后更新时间',
+        selectLanguageName: '简体中文',
+        notFound: '哇哦，没有发现这个页面！',
+        inputPasswordText: '请输入密码',
+        unlockSucessText: '密码正确，玩得开心！',
+        unlockFailuerText: '密码错误，请重新输入！',
+        // 加密整个站点
+        // password: 'a40d453e5ae125ce0ce606b99e82503e',
+        // 设置语言
+        lang: "zh-CN",
+        // 自动设置分类
+        autoSetSeries: true,
+        navbar: [
+            { text: "首页", link: "/" },
+            { text: "分类", link: "/categories/Java/1.html" },
+            { text: "标签", link: "/tags/jishu/1.html" },
             {
-              text: "打赏",
-              link: "/docs/others/donate.html",
+                text: "系列",
+                children: [
+                    { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
+                    { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
+                    {
+                        text: '配置',
+                        children: [
+                            { text: '主题配置', link: '/docs/theme/frontmatter' },
+                            { text: 'Markdown 扩展', link: '/docs/theme/custom-container' },
+                            { text: '高级', link: '/docs/theme/custom-catalog-title' },
+                        ],
+                    },
+                    {
+                        text: '插件',
+                        children: [
+                            { text: 'page', link: '/docs/plugins/page' },
+                            { text: 'comments', link: '/docs/plugins/comments' },
+                            { text: 'vue-previews', link: '/docs/plugins/vue-previews' },
+                            { text: 'bulletin-popover', link: '/docs/plugins/bulletin-popover' },
+                        ],
+                    },
+                ],
             },
-          ],
-        },
-      ],
-    },
-    // commentConfig: {
-    //   type: 'valine',
-    //   // options 与 1.x 的 valineConfig 配置一致
-    //   options: {
-    //     // appId: 'xxx',
-    //     // appKey: 'xxx',
-    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
-    //     // verify: true, // 验证码服务
-    //     // notify: true,
-    //     // recordIP: true,
-    //     // hideComments: true // 隐藏评论
-    //   },
-    // },
-  }),
-  // debug: true,
+            {
+                text: "文档",
+                children: [
+                    { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
+                    { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
+                    {
+                        text: '配置',
+                        children: [
+                            { text: '主题配置', link: '/docs/theme/frontmatter' },
+                            { text: 'Markdown 扩展', link: '/docs/theme/custom-container' },
+                            { text: '高级', link: '/docs/theme/custom-catalog-title' },
+                        ],
+                    },
+                    {
+                        text: '插件',
+                        children: [
+                            { text: 'page', link: '/docs/plugins/page' },
+                            { text: 'comments', link: '/docs/plugins/comments' },
+                            { text: 'vue-previews', link: '/docs/plugins/vue-previews' },
+                            { text: 'bulletin-popover', link: '/docs/plugins/bulletin-popover' },
+                        ],
+                    },
+                ],
+            },
+        ],
+    }),
+    // debug: true,
 });
--- README.md
+++ README.md
@@ -3,7 +3,4 @@ home: true
 modules:
   - Blog
   - Footer
-blog:
-  socialLinks:
-    - { icon: 'LogoGithub', link: 'https://github.com/coriger' }
 ---
