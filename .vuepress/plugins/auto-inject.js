21bbe5f6bc398de8c836d2f774704a453cf110f5
coriger <461504978@qq.com>
2024-11-23 11:28:13 +0800

    优化视频布局

--- .vuepress/plugins/auto-inject.js
+++ .vuepress/plugins/auto-inject.js
@@ -4,7 +4,7 @@ module.exports = (options) => ({
       const originalRender = md.render.bind(md);
       md.render = (src, env) => {
         // 确保 `filePathRelative` 存在并检查路径条件
-        if (env.filePathRelative && env.filePathRelative.includes('series')) {
+        if (env.filePathRelative && env.filePathRelative.includes('/posts/')) {
           const injectedComponent = '<HelloWorld />';
           return originalRender(`${injectedComponent}\n${src}`, env);
         } else {
