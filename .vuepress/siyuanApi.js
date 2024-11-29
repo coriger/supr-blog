e8ce198e92fcb476f4c9e3e3cc53c9686a2e03e1
coriger <461504978@qq.com>
2024-11-29 11:52:19 +0800

    优化

--- .vuepress/siyuanApi.js
+++ .vuepress/siyuanApi.js
@@ -1,22 +1,6 @@
 // siyuanApi.js
 import { SiyuanConfig, SiyuanKernelApi } from "zhi-siyuan-api";
-import { Client } from "@siyuan-community/siyuan-sdk";
 import axios from "axios";
-
-/* 初始化客户端 (默认使用 Axios 发起 XHR 请求) */
-export const siyuanSdk = new Client({
-    /**
-     * (可选) 思源内核服务地址
-     * @default: document.baseURI
-     */
-    baseURL: "https://coriger.cpolar.cn/",
-
-    /**
-     * (可选) 思源内核服务 token
-     * @default: <空>
-     */
-    token: "4si8l21oy72gng6e", // , 默认为空
-});
 
 
 const siyuanConfig = new SiyuanConfig("https://coriger.cpolar.cn", "4si8l21oy72gng6e");
